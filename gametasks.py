5b43c466791c42f726167fef72d9d892d641cb9f
mc11cof <marcalistaircof@gmail.com>
2022-04-15 12:24:49 -0400

    Pushing final project

--- gametasks.py
+++ gametasks.py
@@ -3,15 +3,49 @@
 # Created in April 2022
 # Module holding functions
 
-def printInstructions(insruction):
-    print(insruction)
-    
+# This function prints out the instructions of the game
+def printInstructions(instruction):
+    print(instruction)
+
+
+# This function looks at the username to see if the user already played or if it is his first time
 def getUserScore(userName):
-    file = open("userScores.txt", "r")
-    for n in file:
-        content = file.split()
-        if userName == content:
-            return content
-        else:
-            return "-1"
-    
+    try:
+        input = open("userScores.txt", "r")
+        for line in input:
+            content = line.split(", ")
+            if content[0] == userName:
+                input.close()
+                return content[1]
+        input.close()
+        return "-1"
+    except IOError:
+        print("File not found. A new file will be created.")
+        input = open("userScores.txt", "w")
+        input.close()
+        return "-1"
+
+
+# This function updates the score of the different players based on their username
+def updateUserScore(newUser, userName, score):
+    from os import remove, rename
+
+    if newUser == True:
+        input = open("userScores.txt", "a")
+        input.write(userName + ", " + score + "\n")
+        input.close()
+    else:
+        temp = open("userScores.tmp", "w")
+        input = open("userScores.txt", "r")
+        for line in input:
+            content = line.split(", ")
+            if content[0] == userName:
+                temp.write(userName + ", " + score + "\n")
+            else:
+                temp.write(line)
+
+        input.close()
+        temp.close()
+
+        remove("userScores.txt")
+        rename("userScores.tmp", "userScores.txt")
